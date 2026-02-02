5b0b6eddf74ab07c0a2beadab66d2c521f43e768
MohammadAslamM11M <aslam071595@gmail.com>
2026-02-02 08:56:24 +0530

    2 concepts added for python

---
+++ 01_variables.py
@@ -0,0 +1,5 @@
+a = 1;
+b = 2;
+name = "Mark";
+
+print(f"{name} will add numbers {a} and {b} which will result {a + b}");
---
+++ 02_dataTypes.py
@@ -0,0 +1,14 @@
+#Integers
+a = 12;
+
+#Floating point numbers
+b = 23.34;
+
+#Strings
+name = "Mark";
+
+#Booleans
+status = True;
+
+#None
+value = None;
