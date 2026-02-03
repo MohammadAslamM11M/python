b7fc2e65d3547a9f0ee9068fdcb01c617ae6f6b7
MohammadAslamM11M <aslam071595@gmail.com>
2026-02-03 10:03:24 +0530

    added rules of variables and operators

---
+++ 03_rules_variables.py
@@ -0,0 +1,12 @@
+# Correct way to create variable names
+aaa = 435
+
+mark = 34
+
+sameer = 45
+
+_mark = 34
+
+# Wrong way to create variable names
+# @sameer = 56
+# s@meer = 54
---
+++ 04_operators.py
@@ -0,0 +1,30 @@
+# Arithmetic operators
+a = 7
+b = 4
+c = a + b
+print(c)
+
+# Assignment operators
+a = 4-2     # Assign 4-2 in a
+b = 6
+b *= 3      # Multiply the value of b by 3 and then assign it to b
+print(b)
+
+# Comparison operators
+d = 5 > 4
+print(d)
+
+a = 3 < 4
+print(a)
+
+b = 2 >= 7
+print(b)
+
+c = 5 <= 5
+print(c)
+
+e = 10 == 10
+print(e)
+
+f = 5 != 2
+print(f)
