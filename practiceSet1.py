61ccc5f3787d1fbc85eb7e93bf80b2ecada4662e
MohammadAslamM11M <aslam071595@gmail.com>
2026-02-14 17:25:04 +0530

    practice set 1 for python is added

---
+++ practiceSet1.py
@@ -0,0 +1,37 @@
+# Write a python program to display a user entered name followed by Good Afternoon using input() function
+# username = input("Enter username ")
+
+# print(f"Good Afternoon {username}")
+
+# -----------------------------
+
+# Write a program to fill in a letter template given below with name and date
+# letter = '''Dear <|Name|>,
+# You are selected!
+# <|Date|>'''
+
+# print(letter.replace("<|Name|>", "Mark").replace("<|Date|>", "24 Sep 2050"))
+
+# -----------------------------------
+
+# Write a program to detect double space in a string.
+
+# sentence = "write a  letter"
+
+# print(sentence.find("  "))
+
+# --------------------------------
+
+# Replace double space with single space in above problem
+
+# sentence = "write a  letter"
+
+# print(sentence.replace("  ", " "))
+
+# --------------------------------
+
+# Write a program to format the following letter using escape sequence characters
+
+# letter = "Dear Mark,\n\tThis python course is nice.\nThanks!"
+
+# print(letter)
