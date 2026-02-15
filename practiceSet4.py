4cd68dd47c55d15248f7b1b306d5dfc077fd9015
MohammadAslamM11M <aslam071595@gmail.com>
2026-02-15 13:24:50 +0530

    practise set 4 added

---
+++ practiceSet4.py
@@ -0,0 +1,47 @@
+# Write a program to print 7 fruits given by user
+
+# fruits = []
+
+# for i in range(7):
+#     userinput = input("Enter a fruit name ")
+#     fruits.append(userinput)
+
+# print(fruits)
+
+# -------------------------
+
+# Accept marks of 6 students and display them in a sorted manner
+
+# marks = []
+
+# for i in range(6):
+#     userinput = input("Enter student marks ")
+#     marks.append(userinput)
+
+# marks.sort()
+# print(marks)
+
+# --------------------------------
+
+# Check that a type cannot be changed in python
+
+# a = ("harry", 23, "lesson")
+# a[12] = "larry"
+
+# print(a)
+
+# ---------------------------
+
+# Write a program to sum a list with 4 numbers
+
+# list = [1, 2, 3, 4]
+
+# print(sum(list))
+
+# ----------------------------
+
+# Write a program to count the number of zeros in a tuple
+
+# a = (7, 0, 8, 0, 0, 9)
+
+# print(a.count(0))
